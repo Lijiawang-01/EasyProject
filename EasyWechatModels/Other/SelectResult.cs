c8d90ebbd4ea7bb070f50bd720545279ace1b3bc
mumu <565387073@qq.com>
2025-01-13 16:15:29 +0800

    提交文件

---
+++ EasyWechatModels/Other/SelectResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyWechatModels.Other
+{
+    /// <summary>
+    /// 下拉框结果
+    /// </summary>
+    public class SelectResult
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
